2c3ece6feacc2b50cb8c761c938c7d8c8afa97c7
miltondw <estradamilton2001@gmail.com>
2021-03-24 21:35:08 -0500

    React basic culminado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React,{Component} from 'react';
-import './App.css';
-import Task from './sample/tasks.json'
-console.log(Task)
+
+import './styles/App.scss';
+
+import tasks from './sample/tasks.json';
+
+import Tasks from './components/Tasks';
+import TaskForm from './components/TaskForm';
+
+
 class App extends Component{
+
+  state={
+    tasks:tasks
+  }
+  addTask = (title,description)=>{
+     console.log(title,description)
+  }
   render(){
     return(
       <div>
-        <h1>Tasks</h1>
+        <TaskForm addTask={this.addTask}/>
+      <Tasks task={this.state.tasks} />
       </div>
     );
   }
